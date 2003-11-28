109232d10afbfe4732bcc8b23637b85e01ca9d7a
adedov <>
2003-11-28 03:49:03 +0000

    [project @ 2003-11-28 03:49:03 by adedov] Added.

---
+++ libiqxmlrpc/libiqxmlrpc.h
@@ -0,0 +1,37 @@
+#ifndef _libiqxmlrpc_h_
+#define _libiqxmlrpc_h_
+
+#include <libiqxmlrpc/except.h>
+#include <libiqxmlrpc/value.h>
+#include <libiqxmlrpc/response.h>
+#include <libiqxmlrpc/method.h>
+
+/*! 
+\mainpage libiqxmlrpc Documentation
+  - \ref about
+  - \ref guide
+*/
+
+/*! 
+\page about About
+This library provides Object Oriented API for 
+<a href="http://www.xmlrpc.com>XML-RPC</a>.
+
+Main goals of project is to provide:
+  -# nice object oriented design;
+  -# separate code that operates with XML-RPC structures 
+    and code which provides transport functionality;
+  -# HTTP, HTTPS as default transport;
+  -# make comfortable way to extend library functionality.
+
+\author Anton Dedov <adedov@iqmedia.com> for IQ Media, Inc.
+*/
+
+/*! 
+\page guide Getting started
+\section value Using Value class
+
+Please refer to description of iqxmlrpc::Value class.
+*/
+
+#endif
