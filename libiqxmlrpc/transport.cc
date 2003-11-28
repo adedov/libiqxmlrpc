d833064eca7181fdea1109a1316bbca082c09efe
adedov <>
2003-11-28 10:58:26 +0000

    [project @ 2003-11-28 10:58:26 by adedov] transport.* were added.

---
+++ libiqxmlrpc/transport.cc
@@ -0,0 +1,63 @@
+#include <libxml++/libxml++.h>
+#include <libiqxmlrpc/transport.h>
+#include <libiqxmlrpc/method.h>
+
+using namespace iqxmlrpc;
+
+
+Server::Server( Method_dispatcher* d ):
+  disp(d),
+  request(0),
+  resp_value(0)
+{
+}
+
+
+Server::~Server()
+{
+  delete request;
+}
+
+
+Response Server::execute( const std::string& request_string )
+{
+  std::string error_msg;
+  int fault_code;
+  
+  try {
+    parse_request( request_string );
+    execute();
+    return Response( resp_value );
+  }
+  catch( const xmlpp::exception& e )
+  {
+    error_msg  = e.what();
+    fault_code = Fault_code::xml_parser;
+  }
+  catch( const Exception& e )
+  {
+    error_msg = e.what();
+    fault_code = e.code();
+  }
+
+  return Response( fault_code, error_msg );
+}
+
+
+void Server::parse_request( const std::string& request_string )
+{
+  xmlpp::DomParser parser;
+  parser.set_substitute_entities();
+  parser.parse_memory( request_string );
+
+  delete request;
+  request = new Request( parser.get_document() );
+}
+
+
+void Server::execute()
+{
+  Method* m = disp->create_method( request->get_name() );
+  m->execute( request->get_params(), resp_value );
+  delete m;
+}
---
+++ libiqxmlrpc/transport.h
@@ -0,0 +1,34 @@
+#ifndef _libiqxmlrpc_transport_h_
+#define _libiqxmlrpc_transport_h_
+
+#include <string>
+#include <libiqxmlrpc/request.h>
+#include <libiqxmlrpc/response.h>
+
+
+namespace iqxmlrpc 
+{
+  class Method_dispatcher;
+  class Server;
+  class Client;
+};
+
+
+class iqxmlrpc::Server {
+  Method_dispatcher* disp;
+  Request* request;
+  Value resp_value;
+
+public:
+  Server( Method_dispatcher* );
+  virtual ~Server();
+
+  Response execute( const std::string& xmlrpc_request );
+
+private:
+  void parse_request( const std::string& );
+  void execute();
+};
+
+
+#endif
