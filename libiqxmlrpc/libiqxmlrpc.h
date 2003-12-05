64f4e18224b83304be66bbd3bc9e9a1accc9df87
adedov <>
2003-12-05 07:01:13 +0000

    [project @ 2003-12-05 07:01:13 by adedov] * Add #include "http.h" * Some documention cleaning.

--- libiqxmlrpc/libiqxmlrpc.h
+++ libiqxmlrpc/libiqxmlrpc.h
@@ -1,3 +1,6 @@
+/*! \file libiqxmlrpc.h */
+// This file contains #include macroses and Doxygen docs only.
+
 #ifndef _libiqxmlrpc_h_
 #define _libiqxmlrpc_h_
 
@@ -5,6 +8,7 @@
 #include <libiqxmlrpc/value.h>
 #include <libiqxmlrpc/response.h>
 #include <libiqxmlrpc/method.h>
+#include <libiqxmlrpc/http.h>
 
 /*! 
 \mainpage libiqxmlrpc Documentation
@@ -18,20 +22,17 @@ This library provides Object Oriented API for
 <a href="http://www.xmlrpc.com>XML-RPC</a>.
 
 Main goals of project is to provide:
-  -# nice object oriented design;
-  -# separate code that operates with XML-RPC structures 
-    and code which provides transport functionality;
-  -# HTTP, HTTPS as default transport;
-  -# make comfortable way to extend library functionality.
+  -# Nice object oriented design;
+  -# Maximum to separate code that operates with XML-RPC structures
+     from that one, which provides transport functionality;
+  -# Provide HTTP, HTTPS as standart transports;
+  -# Provide comfortable way to extend library's functionality.
 
 \author Anton Dedov <adedov@iqmedia.com> for IQ Media, Inc.
 */
 
 /*! 
 \page guide Getting started
-\section value Using Value class
-
-Please refer to description of iqxmlrpc::Value class.
 */
 
 #endif
