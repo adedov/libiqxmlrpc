3f1f6ef897f6948c5366f2022be7d47f3e5d914a
adedov <>
2004-03-05 08:51:45 +0000

    [project @ 2004-03-05 08:51:45 by adedov] * Rename Connection_fabric to Accepted_conn_fabric. * Add virtual Serial_conn_fabric::method post_create().

--- libiqnet/conn_fabric.h
+++ libiqnet/conn_fabric.h
@@ -6,30 +6,33 @@
 
 namespace iqnet 
 {
-  class Connection_fabric;
-      
+  class Accepted_conn_fabric;
+    
   template<class Conn_type> 
   class Serial_conn_fabric;
 };
 
 
-//! Interface for creating specific connection's handlers.
-class iqnet::Connection_fabric {
+//! Abstract fabric for accepted connections.
+class iqnet::Accepted_conn_fabric {
 public:
-  virtual ~Connection_fabric() {}
-  
+  virtual ~Accepted_conn_fabric() {}
   virtual void create_accepted( int sock, const Inet_addr& peer_addr ) = 0;
 };
 
 
+//! Fabric for single threaded connections.
 template <class Conn_type>
-class iqnet::Serial_conn_fabric: public iqnet::Connection_fabric {
+class iqnet::Serial_conn_fabric: public iqnet::Accepted_conn_fabric {
 public:
   void create_accepted( int sock, const Inet_addr& peer_addr )
   {
     Conn_type* c = new Conn_type( sock, peer_addr );
+    post_create( c );
     c->post_accept();
   }
+  
+  virtual void post_create( Conn_type* ) {}
 };
 
 #endif
