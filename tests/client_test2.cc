23139222acc3b1d6483443bd91efc220863c6c47
adedov <>
2004-03-15 10:05:05 +0000

    [project @ 2004-03-15 10:05:05 by adedov] Added.

---
+++ tests/client_test2.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <stdexcept>
+#include <libiqxmlrpc/https_transport.h>
+
+using namespace iqxmlrpc;
+using namespace iqnet;
+
+void show_weather( Https_client& client, const std::string& town )
+{
+  Param_list pl;
+  pl.push_back( Value(town) );
+  Response resp = client.execute( "get_weather", pl );
+
+  std::cout << "Site: " << town << std::endl;
+  
+  if( resp.is_fault() )
+  {
+    std::cout << "Fault response: " << resp.fault_string() << std::endl;
+    return;
+  }
+  
+  std::cout 
+    << "Weather: " << resp.value()["weather"].get_string() << "\n"
+    << "Temp:    " << resp.value()["temp.C"].get_double() 
+    << std::endl << std::endl;
+}
+
+
+int main()
+{
+  try {
+    ssl::ctx = ssl::Ctx::client_only();
+    
+    Https_client client( Inet_addr("localhost", 3344) );
+    show_weather( client, "Krasnoyarsk" );
+    show_weather( client, "New York" );
+  }
+  catch ( const std::exception& e )
+  {
+    std::cerr << e.what() << std::endl;
+    return 1;
+  }
+  
+  return 0;
+}
