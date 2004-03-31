75eed50cd7ec06cc00e680c49b5864cdb0efc176
adedov <>
2004-03-31 09:50:45 +0000

    [project @ 2004-03-31 09:50:45 by adedov] Cancel last commit.

--- autogen.sh
+++ autogen.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-aclocal-1.4
-libtoolize-1.4 --force
-automake-1.4 --foreign --add-missing
+aclocal
+libtoolize --force
+automake --foreign --add-missing
 autoconf
 
 echo Please run ./configure now.
