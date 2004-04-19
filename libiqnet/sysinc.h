921badbc508ff5dadd5d1c413b00f0d089ddd57c
adedov <>
2004-04-19 08:37:28 +0000

    [project @ 2004-04-19 08:37:28 by adedov] Added.

---
+++ libiqnet/sysinc.h
@@ -0,0 +1,46 @@
+//  Libiqnet + Libiqxmlrpc - an object-oriented XML-RPC solution.
+//  Copyright (C) 2004 Anton Dedov
+//  
+//  This library is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU Lesser General Public
+//  License as published by the Free Software Foundation; either
+//  version 2.1 of the License, or (at your option) any later version.
+//  
+//  This library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//  Lesser General Public License for more details.
+//  
+//  You should have received a copy of the GNU Lesser General Public
+//  License along with this library; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
+//  
+//  $Id: sysinc.h,v 1.1 2004-04-19 08:37:28 adedov Exp $
+
+/*! \file sysinc.h 
+    This file should help to port library.
+    Insert here include macro of platform dependent headers.
+*/
+
+#ifndef _libiqnet_sysinc_h_
+#define _libiqnet_sysinc_h_
+
+#include <errno.h>
+#include <fcntl.h>
+#include <netdb.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/types.h>
+#include "../config.h"
+
+#ifdef HAVE_POLL
+  #include <sys/poll.h>
+#endif
+
+#endif
