85f85a8bf6afdb2f50ab51f7ae7804367a6ae8a7
adedov <>
2006-08-31 17:21:25 +0000

    [project @ 2006-08-31 17:21:25 by adedov] * Minor code clean-ups. * Use Value utility functions instead of using visitors directly.

--- libiqxmlrpc/request.cc
+++ libiqxmlrpc/request.cc
@@ -1,23 +1,24 @@
 //  Libiqnet + Libiqxmlrpc - an object-oriented XML-RPC solution.
 //  Copyright (C) 2004 Anton Dedov
-//  
+//
 //  This library is free software; you can redistribute it and/or
 //  modify it under the terms of the GNU Lesser General Public
 //  License as published by the Free Software Foundation; either
 //  version 2.1 of the License, or (at your option) any later version.
-//  
+//
 //  This library is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
 //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 //  Lesser General Public License for more details.
-//  
+//
 //  You should have received a copy of the GNU Lesser General Public
 //  License along with this library; if not, write to the Free Software
 //  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
-//  
-//  $Id: request.cc,v 1.6 2006-08-30 18:01:36 adedov Exp $
+//
+//  $Id: request.cc,v 1.7 2006-08-31 17:21:25 adedov Exp $
 
 #include <libxml++/libxml++.h>
+#include <memory>
 #include "request.h"
 #include "parser.h"
 #include "value.h"
@@ -26,13 +27,13 @@
 
 namespace iqxmlrpc {
 
-Request* iqxmlrpc::parse_request( const std::string& request_string )
+Request* parse_request( const std::string& request_string )
 {
   try {
     xmlpp::DomParser parser;
     parser.set_substitute_entities();
     parser.parse_memory( request_string );
-    
+
     return new Request( parser.get_document() );
   }
   catch( xmlpp::exception& e )
@@ -71,29 +72,20 @@ xmlpp::Document* Request::to_xml() const
 {
   using namespace xmlpp;
 
-  Document* doc = new Document();
+  std::auto_ptr<Document> doc(new Document);
 
-  try 
+  Element* el = doc->create_root_node( "methodCall" );
+  Element* name_el = el->add_child( "methodName" );
+  name_el->add_child_text( name );
+  Element* params_el = el->add_child( "params" );
+
+  for( const_iterator i = params.begin(); i != params.end(); ++i )
   {
-    Element* el = doc->create_root_node( "methodCall" );
-    Element* name_el = el->add_child( "methodName" );
-    name_el->add_child_text( name );
-    Element* params_el = el->add_child( "params" );
-    
-    for( const_iterator i = params.begin(); i != params.end(); ++i )
-    {
-      Element* p_el = params_el->add_child( "param" );
-      Value_type_to_xml vis(p_el);
-      i->apply_visitor(vis);
-    }
+    Element* p_el = params_el->add_child( "param" );
+    value_to_xml(*i, p_el);
   }
-  catch(...)
-  {
-    delete doc;
-    throw;
-  }
-  
-  return doc;
+
+  return doc.release();
 }
 
 
@@ -101,7 +93,7 @@ inline void Request::parse( const xmlpp::Node* node )
 {
   if( node->get_name() != "methodCall" )
     throw XML_RPC_violation::at_node(node);
-  
+
   xmlpp::Node::NodeList nlist = Parser::instance()->elements_only( node );
   if( nlist.size() != 2 )
     throw XML_RPC_violation::at_node(node);
@@ -117,15 +109,15 @@ inline void Request::parse_name( const xmlpp::Node* node )
 
   if( node->get_name() != "methodName" )
     throw XML_RPC_violation::at_node(node);
-  
+
   Node::NodeList childs = node->get_children();
   if( childs.size() != 1 )
     throw XML_RPC_violation::at_node(node);
-  
+
   const TextNode *text = dynamic_cast<const TextNode*>(childs.front());
   if( !text )
     throw XML_RPC_violation::at_node(node);
-  
+
   name = text->get_content();
 }
 
@@ -134,16 +126,16 @@ inline void Request::parse_params( const xmlpp::Node* node )
 {
   using namespace xmlpp;
   Parser* parser = Parser::instance();
-  
+
   if( node->get_name() != "params" )
     throw XML_RPC_violation::at_node(node);
-  
+
   Node::NodeList childs = parser->elements_only( node );
   for( Node::NodeList::const_iterator i=childs.begin(); i!=childs.end(); ++i )
   {
     if( (*i)->get_name() != "param" )
       throw XML_RPC_violation::at_node(*i);
-    
+
     Node* param = parser->single_element( *i );
     Value* value = parser->parse_value(param);
     params.push_back( *value );
--- libiqxmlrpc/response.cc
+++ libiqxmlrpc/response.cc
@@ -1,21 +1,21 @@
 //  Libiqnet + Libiqxmlrpc - an object-oriented XML-RPC solution.
 //  Copyright (C) 2004 Anton Dedov
-//  
+//
 //  This library is free software; you can redistribute it and/or
 //  modify it under the terms of the GNU Lesser General Public
 //  License as published by the Free Software Foundation; either
 //  version 2.1 of the License, or (at your option) any later version.
-//  
+//
 //  This library is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
 //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 //  Lesser General Public License for more details.
-//  
+//
 //  You should have received a copy of the GNU Lesser General Public
 //  License along with this library; if not, write to the Free Software
 //  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
-//  
-//  $Id: response.cc,v 1.10 2006-08-30 18:01:36 adedov Exp $
+//
+//  $Id: response.cc,v 1.11 2006-08-31 17:21:25 adedov Exp $
 
 #include <memory>
 #include <libxml++/libxml++.h>
@@ -87,12 +87,12 @@ xmlpp::Document* Response::to_xml() const
 {
   std::auto_ptr<xmlpp::Document> doc( new xmlpp::Document() );
   xmlpp::Element* el = doc->create_root_node( "methodResponse" );
-  
+
   if( is_fault() )
     fault_to_xml( el );
   else
     ok_to_xml( el );
-  
+
   return doc.release();
 }
 
@@ -101,7 +101,7 @@ const Value& Response::value() const
 {
   if( is_fault() )
     throw iqxmlrpc::Exception( fault_string_, fault_code_ );
-  
+
   return *value_;
 }
 
@@ -109,12 +109,12 @@ const Value& Response::value() const
 void Response::parse( const xmlpp::Node* node )
 {
   xmlpp::Node* n = Parser::instance()->single_element( node );
-  
+
   if( n->get_name() == "params" )
     parse_param( n );
   else if( n->get_name() == "fault" )
     parse_fault( n );
-  else 
+  else
     throw XML_RPC_violation::at_node(n);
 }
 
@@ -122,11 +122,11 @@ void Response::parse( const xmlpp::Node* node )
 inline void Response::parse_param( const xmlpp::Node* node )
 {
   Parser *parser = Parser::instance();
-  
+
   xmlpp::Node* param = parser->single_element(node);
   if( param->get_name() != "param" )
     throw XML_RPC_violation::at_node(param);
-  
+
   xmlpp::Node* valnode = parser->single_element(param);
   value_ = parser->parse_value( valnode );
 }
@@ -137,7 +137,7 @@ inline void Response::parse_fault( const xmlpp::Node* node )
   xmlpp::Node* valnode = Parser::instance()->single_element(node);
   std::auto_ptr<Value> fault_(Parser::instance()->parse_value(valnode));
   Value& fault = *fault_.get();
-  
+
   static std::string err( "malformed structure of fault response." );
 
   if( !fault.has_field("faultCode") || !fault.has_field("faultString") )
@@ -154,26 +154,24 @@ inline void Response::parse_fault( const xmlpp::Node* node )
 inline void Response::ok_to_xml( xmlpp::Node* p ) const
 {
   using namespace xmlpp;
-  
+
   Element* params_el = p->add_child( "params" );
   Element* param_el  = params_el->add_child( "param" );
-  Value_type_to_xml vis(param_el);
-  value_->apply_visitor(vis);
+  value_to_xml(*value_, param_el);
 }
 
 
 inline void Response::fault_to_xml( xmlpp::Node* p ) const
 {
   using namespace xmlpp;
-  
+
   Element* fault_el = p->add_child( "fault" );
-  
+
   Struct fault;
   fault.insert( "faultCode", fault_code_ );
   fault.insert( "faultString", fault_string_ );
   Value v( fault );
-  Value_type_to_xml vis(fault_el);
-  v.apply_visitor(vis);
+  value_to_xml(v, fault_el);
 }
 
 } // namespace iqxmlrpc
