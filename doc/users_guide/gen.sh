86ce6bd88b8dc393a86628db868713b783fe1dc8
Anton Dedov <adedov@gmail.com>
2008-05-22 22:30:32 +0700

    Documentation: get rid of serif.css, which causes very small representation of verbatim text.

--- doc/users_guide/gen.sh
+++ doc/users_guide/gen.sh
@@ -2,6 +2,6 @@
 deplate -f htmlslides \
 	-t html-tabbar-right.html \
 	-m html-deplate-button \
-	--css serif --css tabbar-right --css deplate \
+	--css tabbar-right --css deplate \
 	-d html \
 	index.txt
