80256ff8e07258e33e32131c29327ea474a4fd14
Anton Dedov <adedov@gmail.com>
2013-01-27 18:02:36 +0400

    Macport: support Mac OS X in Inet_addr

--- libiqxmlrpc/inet_addr.cc
+++ libiqxmlrpc/inet_addr.cc
@@ -2,7 +2,6 @@
 //  Copyright (C) 2011 Anton Dedov
 
 #include "inet_addr.h"
-
 #include "net_except.h"
 
 using namespace iqnet;
@@ -18,34 +17,47 @@ std::string iqnet::get_host_name()
 }
 
 
+#if defined(_WINDOWS) || defined(__APPLE__)
+#define IQXMLRPC_GETHOSTBYNAME(_h) \
+  hent = ::gethostbyname( _h ); \
+  if( !hent ) { \
+    throw network_error( "gethostbyname" ); \
+  }
+#endif
+
+#if not defined(IQXMLRPC_GETHOSTBYNAME)
+#define IQXMLRPC_GETHOSTBYNAME_PREP \
+  struct hostent hent_local; \
+  char buf[1024]; \
+  int local_h_errno = 0;
+#endif
+
+#if not defined(IQXMLRPC_GETHOSTBYNAME)
+#define IQXMLRPC_GETHOSTBYNAME_POST \
+  if( !hent ) { \
+    throw network_error( "gethostbyname: " + std::string(hstrerror(local_h_errno)), false ); \
+  }
+#endif
+
+#if defined(__sun) || defined(sun)
+#define IQXMLRPC_GETHOSTBYNAME(_h) \
+  IQXMLRPC_GETHOSTBYNAME_PREP \
+  hent = ::gethostbyname_r( _h), &hent_local, buf, sizeof(buf), &local_h_errno ); \
+  IQXMLRPC_GETHOSTBYNAME_POST
+#endif
+
+#if not defined(IQXMLRPC_GETHOSTBYNAME)
+#define IQXMLRPC_GETHOSTBYNAME(_h) \
+  IQXMLRPC_GETHOSTBYNAME_PREP \
+  ::gethostbyname_r( _h, &hent_local, buf, sizeof(buf), &hent, &local_h_errno ); \
+  IQXMLRPC_GETHOSTBYNAME_POST
+#endif
+
 Inet_addr::Inet_addr( const std::string& host_, int port_ ):
   host(host_), port(port_)
 {
   struct hostent* hent = 0;
-
-#ifndef _WINDOWS
-  struct hostent hent_local;
-  char buf[1024];
-  int local_h_errno = 0;
-#if defined(__sun) || defined(sun)
-  hent = ::gethostbyname_r( host.c_str(), &hent_local, buf, sizeof(buf), &local_h_errno );
-#else
-  ::gethostbyname_r( host.c_str(), &hent_local, buf, sizeof(buf), &hent, &local_h_errno );
-#endif
-
-  if( !hent ) {
-    throw network_error( "gethostbyname: " + std::string(hstrerror(local_h_errno)), false );
-  }
-
-#else
-  hent = ::gethostbyname( host.c_str() );
-
-  if( !hent ) {
-    throw network_error( "gethostbyname" );
-  }
-
-#endif
-
+  IQXMLRPC_GETHOSTBYNAME(host.c_str());
   sa.sin_family = PF_INET;
   sa.sin_port = htons(port);
   memcpy( (char*)&sa.sin_addr, (char*)hent->h_addr, hent->h_length );
