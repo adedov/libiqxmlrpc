bd93bbae1cc5efc77f26e22738bbabc3c265ab2e
smiechowy <smiechowy@gmail.com>
2019-09-13 20:39:25 +0200

    Use BOOST_TEST_CHECKPOINT instead of BOOST_CHECKPOINT
    BOOST_CHECKPOINT is deprecated since boost 1.35.0
    https://www.boost.org/doc/libs/1_35_0/libs/test/doc/components/test_tools/reference/deprecated.html

--- tests/client.cc
+++ tests/client.cc
@@ -61,14 +61,14 @@ BOOST_AUTO_TEST_CASE( auth_test )
 {
   BOOST_REQUIRE(test_client);
 
-  BOOST_CHECKPOINT("Successful authorization");
+  BOOST_TEST_CHECKPOINT("Successful authorization");
   test_client->set_authinfo("goodman", "loooooooooooooooooongpaaaaaaaaaaaassssswwwwwwoooooord");
   Response retval( test_client->execute("echo_user", 0) );
   BOOST_CHECK( !retval.is_fault() );
   BOOST_CHECK_EQUAL( retval.value().get_string(), "goodman" );
 
   try {
-    BOOST_CHECKPOINT("Unsuccessful authorization");
+    BOOST_TEST_CHECKPOINT("Unsuccessful authorization");
     test_client->set_authinfo("badman", "");
     retval = test_client->execute("echo_user", 0);
 
